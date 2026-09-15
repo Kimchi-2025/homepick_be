b7747397c2be4fbafef851f76b43ccb09b5e491c
kimch-2025 <jjongbaragi@gmail.com>
2026-09-15 17:51:44 +0900

    수업_0915

--- src/main/java/com/onrender/homepick/controller/HomeController.java
+++ src/main/java/com/onrender/homepick/controller/HomeController.java
@@ -11,6 +11,8 @@ public class HomeController{
 
     @GetMapping("/")
     public String index(HttpSession session, Model model){
+        // 타운캐스팅(강제타입변환: 큰 거 -> 작은 거): Object -> MemberSessionDto
+        // 데이터타입 객체 = (강제타입) session.getAttribut("변수");
         MemberSessionDto user = (MemberSessionDto) session.getAttribute("loginUser");
         model.addAttribute("user", user);
         return "index"; // templates/index.html
